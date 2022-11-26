f4a5edc1cf93a6c959f796e4a1072377d3ac2d38
orn974@gmail.com <Grigorik>
2022-11-26 13:15:45 +0300

    Angular begin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {GetComponent} from "./Components/store-list/get.component";
 import {HttpClientModule} from "@angular/common/http";
+import {AlertModule} from "ngx-bootstrap/alert";
+
+
+
 
 @NgModule({
   declarations: [
@@ -14,7 +18,8 @@ import {HttpClientModule} from "@angular/common/http";
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
